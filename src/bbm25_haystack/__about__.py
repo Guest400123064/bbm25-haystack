3b41ef773e8945b30f09f1624149df6b3f22f3ef
guest400123064 <lgfz1003@icloud.com>
2024-04-27 15:48:30 -0400

    try remove typing extension from dependency and try running on py312 env

--- src/bbm25_haystack/__about__.py
+++ src/bbm25_haystack/__about__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024-present Yuxuan Wang <wangy49@seas.upenn.edu>
+# SPDX-FileCopyrightText: 2024-present Guest400123064 <wangy49@seas.upenn.edu>
 #
 # SPDX-License-Identifier: Apache-2.0
 
